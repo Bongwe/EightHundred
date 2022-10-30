f1f59da7584693ea8a11d4a9927419e1f6aa2b50
Mzwandile <obomvu@gmail.com>
2022-10-30 09:10:42 +0200

    The house can shoot now

--- Assets/Scripts/Level1/ShootingShack.cs
+++ Assets/Scripts/Level1/ShootingShack.cs
@@ -12,7 +12,7 @@ public class ShootingShack : MonoBehaviour
 	private ParticleSystem explosionFX;     // Reference to the particle system of the explosion effect.
 	public int damage = 4;
 	public float bulletSpeed = 20;
-	private bool isShooting = false;
+	private bool isShooting = true;
 	public float ammunition = 10;
 	public AudioClip[] gunSound;              
 
@@ -21,8 +21,6 @@ public class ShootingShack : MonoBehaviour
 		// Setting up references.
 		//explosionFX = GameObject.FindGameObjectWithTag("ExplosionFX").GetComponent<ParticleSystem>();
 
-		// Destroy the rocket after 8 seconds if it doesn't get destroyed before then.
-		Destroy(gameObject, 2);
 	}
 
 	void OnExplode()
@@ -36,21 +34,21 @@ public class ShootingShack : MonoBehaviour
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
-		// Otherwise if it hits a Shack...
-		if (col.tag == "Bullet")
-		{
-			// ... find the Enemy script and call the Hurt function.
-			//col.gameObject.GetComponent<Shack>().pistolDamage(damage);
+        // Otherwise if it hits a Shack...
+        if (col.tag == "Bullet")
+        {
+            // ... find the Enemy script and call the Hurt function.
+            //col.gameObject.GetComponent<Shack>().pistolDamage(damage);
 
-			// Call the explosion instantiation.
-			//Do not destroy the bullet if the shack has not been destroyed
-			if (!col.gameObject.GetComponent<Shack>().shackIsDestroyed)
-			{
-				OnExplode();
-			}
-		}
+            // Call the explosion instantiation.
+            //Do not destroy the bullet if the shack has not been destroyed
+            if (!col.gameObject.GetComponent<Shack>().shackIsDestroyed)
+            {
+                OnExplode();
+            }
+        }
 
-	}
+    }
 
 	public void Explode()
 	{
@@ -59,28 +57,30 @@ public class ShootingShack : MonoBehaviour
 
     public void Update()
     {
-		// If the fire button is pressed...
-		if (isShooting && ammunition > 0)
-		{
-			// ... set the animator Shoot trigger parameter and play the audioclip.
-			//anim.SetTrigger("Shoot");
-			//GetComponent<AudioSource>().Play();
-			AudioSource.PlayClipAtPoint(gunSound[0], transform.position);
-			ammunition--;
+        // If the fire button is pressed...
+        if (isShooting && ammunition > 0)
+        {
+            // ... set the animator Shoot trigger parameter and play the audioclip.
+            //anim.SetTrigger("Shoot");
+            //GetComponent<AudioSource>().Play();
+            AudioSource.PlayClipAtPoint(gunSound[0], transform.position);
+            ammunition--;
 
-			Vector2 bulletSpawnPosition = transform.position;
+            Vector2 bulletSpawnPosition = transform.position;
 
-			if (isShooting)
-			{
-				// Otherwise instantiate the rocket facing left and set it's velocity to the left.
-				Rigidbody2D bulletInstance = Instantiate(weapon, transform.position, Quaternion.Euler(new Vector3(0, 0, 180f))) as Rigidbody2D;
-				bulletInstance.velocity = new Vector2(bulletSpeed, 0);
-			}
+            if (isShooting)
+            {
+                Vector3 shackPosition = transform.position;
+				//shackPosition.x = shackPosition.x - 100f;
+			   // Otherwise instantiate the rocket facing left and set it's velocity to the left.
+				Rigidbody2D bulletInstance = Instantiate(weapon, shackPosition, Quaternion.Euler(new Vector3(0, 0, 180f))) as Rigidbody2D;
+                bulletInstance.velocity = new Vector2( -1 * bulletSpeed, 0);
+            }
 
-		}
-		
+        }
 
-		isShooting = false;
+
+        //isShooting = false;
 	}
 
 	public void shoot()
